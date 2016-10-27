c6a0f96380f44578c501271c248d98e9782a07e7
Michael Stocks <mspopgen@gmail.com>
2016-10-27 15:01:43 +0100

    Test implementation of pqgen het
    - Calculates average heterozygosity per factor
    - Deals only with 0/1 genotype information

---
+++ src/pq_het.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pq_args.h>
+#include <pq_generics.h>
+
+void pq_swupdate_het(struct SWrap *wrap, char **array)
+{
+  int i;
+  int n;
+  int nhet;
+  char *curr;
+  double het;
+
+  n = 0;
+  het = 0.0;
+  nhet = 0;
+  for (i = 0; i < NKARGS; i++) {
+    curr = array[KCOLS[i]];
+    if (curr[0] != '.') {
+      if (curr[0] == '0' && curr[2] == '1') {
+	nhet++;
+      }
+      n++;
+    }
+  }
+  het = (double)nhet / (double)n;
+  if (n > 0) {
+    *(long long int *)wrap->values[0] += 1;
+    *(double *)wrap->values[1] += het;
+  }
+}
+
+void pq_swwrite_het(struct SWrap *wrap)
+{
+  double het;
+  sprintf(wrap->outs[0], "%d", wrap->nsam);
+  sprintf(wrap->outs[1], "%lli", *(long long int *)wrap->values[0]);
+  het = *(double *)wrap->values[1] / *(long long int *)wrap->values[0];
+  sprintf(wrap->outs[2], "%f", het);
+}
+
+void pq_swclear_het(struct SWrap *wrap)
+{
+  *(long long int *)wrap->values[0] = 0;
+  *(double *)wrap->values[1] = 0.0;
+}
+
+// nvalues = [nvsites, het]
+// nouts   = [nsam, nvsites, uhet]
+
+void pq_swinit_het(struct SWrap *wrap, int nsam)
+{
+  int i;
+  wrap->nsam = nsam;
+  wrap->nouts = 3;
+  wrap->nvalues = 2;
+  wrap->outs = calloc(wrap->nouts, sizeof(char *));
+  wrap->values = calloc(wrap->nvalues, sizeof(void *));
+  for (i = 0; i < wrap->nouts; i++) {
+    wrap->outs[i] = calloc(128, sizeof(char));
+  }
+  
+  wrap->values[0] = malloc(sizeof(long long int));
+  *(long long int *)wrap->values[0] = 0;
+    wrap->values[1] = malloc(sizeof(double));
+  *(double *)wrap->values[1] = 0.0;
+  
+  wrap->update = pq_swupdate_het;
+  wrap->write = pq_swwrite_het;
+  wrap->clear = pq_swclear_het;
+}
+
