2ca8b794f52764406fa08ae3679b80a853005b7c
Michael Stocks <mspopgen@gmail.com>
2016-11-10 09:04:45 +0000

    Standardised --help and command unrecognised routines
    - Each command header (pq_<cmd>.h) now has PQ_<cmd>_NAME and
      PQ_<cmd>_DESC
    - Messages follow same format as git --help

--- include/pq_theta.h
+++ include/pq_theta.h
@@ -1,6 +1,9 @@
 #ifndef pq_theta_h__
 #define pq_theta_h__
 
+#define PQ_THETA_NAME "theta"
+#define PQ_THETA_DESC "Calculate site frequency based statistics"
+
 extern void nref_nalt(void **counts, char **array);
 extern void pq_swinit_theta(struct SWrap *wrap, int nsam);
 
